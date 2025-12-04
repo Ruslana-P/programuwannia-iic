e2f2def9feecb8b7f9ee404c56a3e5782486903c
P-Ruslana <pavliuk_r_v@protonmail.com>
2025-12-04 12:58:59 +0100

    commit 7 - add debug tools

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -26,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar"
 ]
 
 MIDDLEWARE = [
@@ -36,6 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -109,3 +110,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path("polls/", include("polls.urls")), # use include() for including other URL patterns
     path('admin/', admin.site.urls), # pre-built URLconf provided by Django for the default admin site.
 ]
+
+# --- DEBUG TOOLBAR CONFIGURATION (Conditional Inclusion) ---
+if settings.DEBUG:
+    # 1. Import debug_toolbar
+    import debug_toolbar 
+    
+    # 2. Prepend the debug toolbar's URLs to urlpatterns
+    #    The 'debug_toolbar.urls' module contains the URL patterns list.
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
